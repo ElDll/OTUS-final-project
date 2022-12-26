ddfc5f2a8f10b8ad8578e66ea603a8eef6bc55c0
aleksei.kolchanov <aleksei.kolchanov@simbirsoft.com>
2022-12-27 00:30:47 +0400

    -

--- launch.bat
+++
@@ -1,4 +0,0 @@
-docker build -t tests .
-docker run --name tests_run --network my_network tests --executor %executor% --browser %browser% --url %opencart_address% -n %threads% --bv %bv%
-docker cp tests_run:/app/allure-results .
-docker rm tests_run
