80cb4e64fbe911626ca652e12acf2ed661f1515a
aleksei.kolchanov <aleksei.kolchanov@simbirsoft.com>
2022-12-26 18:16:25 +0400

    -

--- launch.bat
+++ launch.bat
@@ -1,3 +1,3 @@
 docker build -t tests .
-docker run --rm --name tests_run --network my_network tests
+docker run --name tests_run --network my_network tests
 docker cp tests_run:/app/allure-results .
